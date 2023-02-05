a4b46ef80be1de923e23e90af5fd30d1797ae157
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-02-06 03:37:44 +0600

    first

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,5 +1,19 @@
-function number(){
-    return 10;
+//function
+function num() {
+    document.write(100);
 }
+num();
 
+//arrow function
+let number = () => {
+    return 10;
+};
 console.log(number());
+
+//we can write it this way also
+let num2 = () => console.log(5);
+num2();
+
+//function using parameters
+let num3 = (a,b) => a + b;
+console.log(num3(20,30));
